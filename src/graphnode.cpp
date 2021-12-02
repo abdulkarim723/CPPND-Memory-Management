2addcb24ef852c4b49379b5576d509c77b1e4704
Abdulkarim Saddik <abdulkarim.saddik@gmail.com>
2021-12-02 08:36:00 +0100

    getting forward

--- src/graphnode.cpp
+++ src/graphnode.cpp
@@ -6,15 +6,6 @@ GraphNode::GraphNode(int id)
     _id = id;
 }
 
-GraphNode::~GraphNode()
-{
-    //// STUDENT CODE
-    ////
-    //delete _chatBot; 
-
-    ////
-    //// EOF STUDENT CODE
-}
 
 void GraphNode::AddToken(std::string token)
 {
@@ -26,9 +17,9 @@ void GraphNode::AddEdgeToParentNode(GraphEdge *edge)
     _parentEdges.push_back(edge);
 }
 
-void GraphNode::AddEdgeToChildNode(GraphEdge *edge)
+void GraphNode::AddEdgeToChildNode(std::unique_ptr<GraphEdge> edge)
 {
-    _childEdges.push_back(edge);
+    _childEdges.push_back(std::move(edge));
 }
 
 //// STUDENT CODE
@@ -47,12 +38,12 @@ void GraphNode::MoveChatbotToNewNode(GraphNode *newNode)
 ////
 //// EOF STUDENT CODE
 
-GraphEdge *GraphNode::GetChildEdgeAtIndex(int index)
+std::unique_ptr<GraphEdge> GraphNode::GetChildEdgeAtIndex(int index)
 {
     //// STUDENT CODE
     ////
 
-    return _childEdges[index];
+    return std::move(_childEdges[index]);
 
     ////
     //// EOF STUDENT CODE
--- src/graphnode.h
+++ src/graphnode.h
@@ -4,6 +4,8 @@
 #include <vector>
 #include <string>
 #include "chatbot.h"
+#include "iostream"
+#include "memory"
 
 
 // forward declarations
@@ -16,7 +18,7 @@ private:
     ////
 
     // data handles (owned)
-    std::vector<GraphEdge *> _childEdges;  // edges to subsequent nodes
+    std::vector<std::unique_ptr<GraphEdge>> _childEdges;  // edges to subsequent nodes
 
     // data handles (not owned)
     std::vector<GraphEdge *> _parentEdges; // edges to preceding nodes 
@@ -32,19 +34,19 @@ private:
 public:
     // constructor / destructor
     GraphNode(int id);
-    ~GraphNode();
+    //~GraphNode();
 
     // getter / setter
     int GetID() { return _id; }
     int GetNumberOfChildEdges() { return _childEdges.size(); }
-    GraphEdge *GetChildEdgeAtIndex(int index);
+    std::unique_ptr<GraphEdge> GetChildEdgeAtIndex(int index);
     std::vector<std::string> GetAnswers() { return _answers; }
     int GetNumberOfParents() { return _parentEdges.size(); }
 
     // proprietary functions
     void AddToken(std::string token); // add answers to list
     void AddEdgeToParentNode(GraphEdge *edge);
-    void AddEdgeToChildNode(GraphEdge *edge);
+    void AddEdgeToChildNode(std::unique_ptr<GraphEdge> edge);
 
     //// STUDENT CODE
     ////
